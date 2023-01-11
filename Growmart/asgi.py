f5a420bb4935be40bac128e189d4cbd85d08cc60
yogesh <yogesh.jaguri2401@gmail.com>
2023-01-11 16:27:53 +0530

    update

--- Growmart/asgi.py
+++ Growmart/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for growmart project.
+ASGI config for Growmart project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'growmart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Growmart.settings')
 
 application = get_asgi_application()
